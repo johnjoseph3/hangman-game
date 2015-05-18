1a6cfa70237623f809fe6279358052ca69f061e3
John Joseph <jmj1444@gmail.com>
2015-05-17 22:10:51 -0400

    updating gh pages

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 var hangmanWords = [
-  "the","of","and","a","to","in","is","you","that","it","he",
-  "was","for","on","are","as","with","his","they","I","at","be",
-  "this","have","from","or","one","had","by","word","but","not",
-  "what","all","were","we","when","your","can","said","there",
-  "use","an","each","which","she","do","how","their","if","will",
-  "up","other","about","out","many","then","them","these","so",
-  "some","her","would","make","like","him","into","time","has",
-  "look","two","more","write","go","see","number","no","way",
-  "could","people","my","than","first","water","been","call",
-  "who","oil","its","now","find","long","down","day","did","get",
-  "come","made","may","part"
+  "fanciful","gentle","air","float","breeze","sleep","sound","float","drift","rest","myth",
+  "mystical","magic","awake","drift","sing","breath","calm","muse","mist","day","be",
+  "astral","ideal","relax","aware","ideal","fresh","vague","vision","perspective","shadow",
+  "inner","sooth","believe","lull","substance","jolly","agree","enjoy","reach",
+  "delve","beauty","different","same","one","all","inside","outside","within","without",
+  "cheer","divert","forget","sedate","numb","look","airy","ethereal","light",
+  "refine","simple","aeriel","vapor","mist","celestial","rare","time","charm",
+  "slight","soft","mild","fine","number","people","water","call",
+  "find"
 ];
 
 
@@ -60,7 +58,7 @@ resetButton.addEventListener('click', function() {
 } );
 
 
-userAttempts = 8;
+userAttempts = 10;
 
 guessOutput = document.querySelector('.attempt-counter');
 guessOutput.innerHTML = "You have: " + userAttempts + " guesses left";   
